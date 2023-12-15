998762c73e543f20cb075b8d18b6cefb4cf219b5
PonyCui <cuis@vip.qq.com>
2023-12-15 10:21:57 +0000

    fix: Storage type

--- src/wechat_flutter_js/flutter_bom/storage.js
+++ src/wechat_flutter_js/flutter_bom/storage.js
@@ -1,26 +1,23 @@
 class Storage {
   constructor() {
+    this.$$clazz$$ = "Storage";
     this.$_keys = [];
   }
-
+  
   get length() {
     return (this.$_keys && this.$_keys.length) || 0;
   }
-
   key(num) {
     if (typeof num !== "number" || !isFinite(num) || num < 0) return null;
     return this.$_keys[num] || null;
   }
-}
 
-class LocalStorage extends Storage {
   set $$keys(keys) {
     this.$_keys = keys;
   }
 
   getItem(key) {
     if (!key || typeof key !== "string") return null;
-
     return wx.getStorageSync(key) || null;
   }
 
@@ -38,8 +35,7 @@ class LocalStorage extends Storage {
   clear() {
     wx.clearStorageSync();
   }
+  
 }
 
-module.exports = {
-  LocalStorage,
-};
+module.exports = { LocalStorage: Storage };
