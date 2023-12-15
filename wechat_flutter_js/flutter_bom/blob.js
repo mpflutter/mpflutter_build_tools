ff7d5e5f8561bc420440a4758d5a223b241041ec
PonyCui <cuis@vip.qq.com>
2023-12-15 11:20:43 +0000

    fix: storage keys

--- wechat_flutter_js/flutter_bom/blob.js
+++ wechat_flutter_js/flutter_bom/blob.js
@@ -1,1 +1,50 @@
-function mergeArrayBuffers(buffers){let totalLength=0;for(let index=0;index<buffers.length;index++){const buffer=buffers[index];totalLength+=buffer.byteLength}const mergedBuffer=new ArrayBuffer(totalLength);const mergedView=new Uint8Array(mergedBuffer);let offset=0;for(let index=0;index<buffers.length;index++){const buffer=buffers[index];const view=new Uint8Array(buffer);mergedView.set(view,offset);offset+=view.length}return mergedBuffer}export class Blob{constructor(blobParts,type){this.$$clazz$$="Blob";this.originArrayBuffer=mergeArrayBuffers(blobParts);this.size=this.originArrayBuffer.byteLength;this.type=type}arrayBuffer(){return this.originArrayBuffer}text(){const tmpFile=wx.env.USER_DATA_PATH+"/brtext_tmp";fs.writeFileSync(tmpFile,this.originArrayBuffer);const localFileText=fs.readFileSync(tmpFile,"utf8");fs.removeSavedFile({filePath:tmpFile});return localFileText}}
+function mergeArrayBuffers(buffers) {
+    // 计算总长度
+    let totalLength = 0;
+  
+    for (let index = 0; index < buffers.length; index++) {
+        const buffer = buffers[index];
+        totalLength += buffer.byteLength;
+    }
+  
+    // 创建一个新的ArrayBuffer来容纳合并后的数据
+    const mergedBuffer = new ArrayBuffer(totalLength);
+  
+    // 使用TypedArray视图或DataView来操作二进制数据
+    const mergedView = new Uint8Array(mergedBuffer);
+  
+    // 拷贝每个ArrayBuffer的数据到合并后的ArrayBuffer
+    let offset = 0;
+    for (let index = 0; index < buffers.length; index++) {
+      const buffer = buffers[index];
+      const view = new Uint8Array(buffer);
+      mergedView.set(view, offset);
+      offset += view.length;
+    }
+  
+    return mergedBuffer;
+  }
+  
+  export class Blob {
+    constructor(blobParts, type) {
+      this.$$clazz$$ = "Blob";
+      this.originArrayBuffer = mergeArrayBuffers(blobParts);
+      this.size = this.originArrayBuffer.byteLength;
+      this.type = type;
+    }
+  
+    arrayBuffer() {
+      return this.originArrayBuffer;
+    }
+  
+    text() {
+      const tmpFile = wx.env.USER_DATA_PATH + "/brtext_tmp";
+      fs.writeFileSync(tmpFile, this.originArrayBuffer);
+      const localFileText = fs.readFileSync(tmpFile, "utf8");
+      fs.removeSavedFile({
+        filePath: tmpFile,
+      });
+      return localFileText;
+    }
+  }
+  
