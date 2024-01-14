712e58486a80c53dcb92e236acbfe761da3e7518
崔 明辉 <cuis@vip.qq.com>
2024-01-14 15:53:00 +0800

    fix: crypto value

--- wechat_flutter_js/flutter_bom/crypto.js
+++ wechat_flutter_js/flutter_bom/crypto.js
@@ -2,37 +2,54 @@
 // Use of this source code is governed by a Apache License Version 2.0 that can be
 // found in the LICENSE file.
 
-let lastRandomValues = undefined;
+let randomValuesBuf = [];
 
-export const genRandomValues = () => {
-  if (wx && wx.getRandomValues) {
-    wx.getRandomValues({
-      length: 32,
-      success: (res) => {
-        lastRandomValues = new Uint8Array(res.randomValues);
-      },
-    });
+export const genRandomValues = (forceUseMock, batchCount) => {
+  if (!forceUseMock && wx && wx.getRandomValues) {
+    if (batchCount !== undefined) {
+      wx.getRandomValues({
+        length: 32 * batchCount,
+        success: (res) => {
+          let randomValuesUint8Array = new Uint8Array(res.randomValues);
+          for (let index = 0; index < 32 * batchCount; index += 32) {
+            randomValuesBuf.push(
+              new Uint8Array(randomValuesUint8Array.subarray(index, index + 32))
+            );
+          }
+        },
+      });
+    } else {
+      wx.getRandomValues({
+        length: 32,
+        success: (res) => {
+          randomValuesBuf.push(new Uint8Array(res.randomValues));
+        },
+      });
+    }
   } else {
-    console.warn("不存在 wx.getRandomValues 接口，正在使用伪随机数。");
     let arr = [];
     for (let index = 0; index < 32; index++) {
       arr.push(Math.floor(Math.random() * 255));
     }
-    lastRandomValues = new Uint8Array(arr);
+    randomValuesBuf.push(new Uint8Array(arr));
   }
 };
 
 export const randomFillSync = (buffer) => {
-  genRandomValues();
-  if (lastRandomValues) {
-    for (let index = 0; index < lastRandomValues.length; index++) {
-      buffer[index] = lastRandomValues[index];
+  if (randomValuesBuf.length < 1) {
+    genRandomValues(true);
+  }
+  const currentValue = randomValuesBuf.shift();
+  if (currentValue) {
+    for (let index = 0; index < currentValue.length; index++) {
+      buffer[index] = currentValue[index];
     }
   }
+  genRandomValues(false);
 };
 
 export const getRandomValues = (ab) => {
   randomFillSync(ab);
 };
 
-genRandomValues();
+genRandomValues(false, 1024);
